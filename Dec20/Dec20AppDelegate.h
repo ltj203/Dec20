6de2a51e18d51dc17129c25bbb3a6a40754f7220
Lisa <ljenkin2@hotmail.com>
2012-12-20 00:05:52 -0500

    Initial Commit

---
+++ Dec20/Dec20AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Dec20AppDelegate.h
+//  Dec20
+//
+//  Created by Lisa Jenkins on 12/20/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Dec20AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dec20/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dec20
+//
+//  Created by Lisa Jenkins on 12/20/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Dec20AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Dec20AppDelegate class]));
+    }
+}
